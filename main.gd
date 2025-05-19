d51eee02e17736a36cfed2e3e59f112123c3172a
Antz <anthony.iantria@gmail.com>
2025-05-19 07:32:30 -0400

    Made into plugin.

--- main.gd
+++ main.gd
@@ -1,34 +1,32 @@
 extends Node3D
 
 @export_category("A* Search")
-@export var grid_map_cell_size: int = 1
-@export var grid_walkable_items: Array[int] = [0] # what is the id of the walkable tile type in GridMap
 @export var start_tile: Vector3i = Vector3i(-2,0,24)
 @export var end_tile: Vector3i = Vector3i(4,0,0)
 @export var test_tile: Vector3i = Vector3i(-2,0,18)
-@onready var grid_map: GridMap = $GridMap
+@onready var grid_map: GridMapPathFinding = $GridMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GridMapPathFinding.setup_astar_grid(grid_map, grid_walkable_items, grid_map_cell_size)
+	grid_map.setup_astar_grid(grid_map.walkable_items)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	GridMapPathFinding.do_debug_path(grid_map, grid_map_cell_size, start_tile, end_tile)
+	grid_map.do_debug_path(start_tile, end_tile)
 
 func _on_remove_tile_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		grid_map.set_cell_item(test_tile, grid_walkable_items[0], 0) # stone floor tile, walkable
+		grid_map.set_cell_item(test_tile, grid_map.walkable_items[0], 0) # stone floor tile, walkable
 	else:
 		grid_map.set_cell_item(test_tile, 2, 0) # 2 is a wall
-	GridMapPathFinding.setup_astar_grid(grid_map, grid_walkable_items, grid_map_cell_size)
+	grid_map.setup_astar_grid(grid_map.walkable_items)
 
 func _on_add_wood_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		grid_walkable_items = [0,1] # stone and wood floor tile
+		grid_map.walkable_items = [0,1] # stone and wood floor tile
 	else:
-		grid_walkable_items = [0] # just stone floor tile
-	GridMapPathFinding.setup_astar_grid(grid_map, grid_walkable_items, grid_map_cell_size)
+		grid_map.walkable_items = [0] # just stone floor tile
+	grid_map.setup_astar_grid(grid_map.walkable_items)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("ui_cancel"):
